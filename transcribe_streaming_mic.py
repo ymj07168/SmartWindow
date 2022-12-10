b7dffebdfb0a82527f7787df6cefad6547bf658e
이효원 <ymj07168@gmail.com>
2022-12-10 23:29:36 +0900

    [Test]특정 음성명령 포함여부 테스트

--- transcribe_streaming_mic.py
+++ transcribe_streaming_mic.py
@@ -154,6 +154,9 @@ def listen_print_loop(responses):
 
         else:
             print(transcript + overwrite_chars)
+            if "안녕" in transcript:
+                print("ㅎㅎㅎㅎㅎㅎ")
+
 
             # Exit recognition if any of the transcribed phrases could be
             # one of our keywords.
