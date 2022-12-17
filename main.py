37c35e4265fd74b31f62fc7952e208726ee83546
이효원 <ymj07168@gmail.com>
2022-12-17 19:05:17 +0900

    [Feat] 쓰레드 메인 코드

--- main.py
+++ main.py
@@ -1,30 +1,278 @@
-import RPi.GPIO as gpio
-import DCmotor
-import LCD
+from __future__ import division
+import re
+import sys
 
-from time import sleep
-from gpiozero import InputDevice
+from google.cloud import speech
 
-LCD_LINE_1 = 0x80   # LCD RAM address for the 1st line
-LCD_LINE_2 = 0xC0
+import pyaudio
+from six.moves import queue
 
-
+from multiprocessing import Process, Queue
+import sensor
+import time
 
 is_open = 0
 
-while True:
-    LCD.lcd_init()
-    gpio.setmode(gpio.BCM)
-    no_rain = InputDevice(26)
+#!/usr/bin/env python
 
-    if not no_rain.is_active:
-        print("It's raining - get the washing in!")
-        LCD.lcd_string("raining", LCD_LINE_1)
-        # DCmotor.forward(1)
-    else:
-        print("NO rain")
-        LCD.lcd_string("no rain", LCD_LINE_2)
-        # DCmotor.reverse(1)
+# Copyright 2017 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-        is_open = 0
-    sleep(1)
+"""Google Cloud Speech API sample application using the streaming API.
+
+NOTE: This module requires the additional dependency `pyaudio`. To install
+using pip:
+
+    pip install pyaudio
+
+Example usage:
+    python transcribe_streaming_mic.py
+"""
+
+# [START speech_transcribe_streaming_mic]
+
+
+# Audio recording parameters
+RATE = 16000
+CHUNK = int(RATE / 10)  # 100ms
+
+order = "명령"
+
+class MicrophoneStream(object):
+    """Opens a recording stream as a generator yielding the audio chunks."""
+
+    def __init__(self, rate, chunk):
+        self._rate = rate
+        self._chunk = chunk
+
+        # Create a thread-safe buffer of audio data
+        self._buff = queue.Queue()
+        self.closed = True
+
+    def __enter__(self):
+        self._audio_interface = pyaudio.PyAudio()
+        self._audio_stream = self._audio_interface.open(
+            format=pyaudio.paInt16,
+            # The API currently only supports 1-channel (mono) audio
+            # https://goo.gl/z757pE
+            channels=1,
+            rate=self._rate,
+            input=True,
+            frames_per_buffer=self._chunk,
+            # Run the audio stream asynchronously to fill the buffer object.
+            # This is necessary so that the input device's buffer doesn't
+            # overflow while the calling thread makes network requests, etc.
+            stream_callback=self._fill_buffer,
+        )
+
+        self.closed = False
+
+        return self
+
+    def __exit__(self, type, value, traceback):
+        self._audio_stream.stop_stream()
+        self._audio_stream.close()
+        self.closed = True
+        # Signal the generator to terminate so that the client's
+        # streaming_recognize method will not block the process termination.
+        self._buff.put(None)
+        self._audio_interface.terminate()
+
+    def _fill_buffer(self, in_data, frame_count, time_info, status_flags):
+        """Continuously collect data from the audio stream, into the buffer."""
+        self._buff.put(in_data)
+        return None, pyaudio.paContinue
+
+    def generator(self):
+        while not self.closed:
+            # Use a blocking get() to ensure there's at least one chunk of
+            # data, and stop iteration if the chunk is None, indicating the
+            # end of the audio stream.
+            chunk = self._buff.get()
+            if chunk is None:
+                return
+            data = [chunk]
+
+            # Now consume whatever other data's still buffered.
+            while True:
+                try:
+                    chunk = self._buff.get(block=False)
+                    if chunk is None:
+                        return
+                    data.append(chunk)
+                except queue.Empty:
+                    break
+
+            yield b"".join(data)
+
+
+def listen_print_loop(responses):
+    """Iterates through server responses and prints them.
+
+    The responses passed is a generator that will block until a response
+    is provided by the server.
+
+    Each response may contain multiple results, and each result may contain
+    multiple alternatives; for details, see https://goo.gl/tjCPAU.  Here we
+    print only the transcription for the top alternative of the top result.
+
+    In this case, responses are provided for interim results as well. If the
+    response is an interim one, print a line feed at the end of it, to allow
+    the next result to overwrite it, until the response is a final one. For the
+    final one, print a newline to preserve the finalized transcription.
+    """
+    num_chars_printed = 0
+
+
+    for response in responses:
+        if not response.results:
+            continue
+
+        # The `results` list is consecutive. For streaming, we only care about
+        # the first result being considered, since once it's `is_final`, it
+        # moves on to considering the next utterance.
+        result = response.results[0]
+        if not result.alternatives:
+            continue
+
+        # Display the transcription of the top alternative.
+        # global transcript
+        transcript = result.alternatives[0].transcript
+
+
+        # Display interim res        # global order
+        #         # order = transcriptults, but with a carriage return at the end of the
+        # line, so subsequent lines will overwrite them.
+        #
+        # If the previous result was longer than this one, we need to print
+        # some extra spaces to overwrite the previous result
+        overwrite_chars = " " * (num_chars_printed - len(transcript))
+
+        if not result.is_final:
+            sys.stdout.write(transcript + overwrite_chars + "\r")
+            sys.stdout.flush()
+
+            num_chars_printed = len(transcript)
+
+        else:
+            print(transcript + overwrite_chars)
+            global order
+            order = transcript
+            print(order + "!")
+
+            # # Exit recognition if any of the transcribed phrases could be
+            # # one of our keywords.
+            if re.search(r"\b(exit|quit)\b", transcript, re.I):
+                print("Exiting..")
+                break
+
+            num_chars_printed = 0
+
+
+def main():
+    # See http://g.co/cloud/speech/docs/languages
+    # for a list of supported languages.
+    language_code = "ko-KR"  # a BCP-47 language tag
+
+    client = speech.SpeechClient()
+    config = speech.RecognitionConfig(
+        encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
+        sample_rate_hertz=RATE,
+        language_code=language_code,
+    )
+
+    streaming_config = speech.StreamingRecognitionConfig(
+        config=config, interim_results=True
+    )
+
+    with MicrophoneStream(RATE, CHUNK) as stream:
+        audio_generator = stream.generator()
+        requests = (
+            speech.StreamingRecognizeRequest(audio_content=content)
+            for content in audio_generator
+        )
+
+        responses = client.streaming_recognize(streaming_config, requests)
+
+        # Now, put the transcription responses to use.
+        listen_print_loop(responses)
+
+def test():
+    global order
+    while 1:
+        print(order)
+        time.sleep(1)
+
+if __name__ == "__main__":
+    print("스마트 창문 시작")
+    th = Process(target=main)
+    th.start()
+    print("음성인식기능 시작")
+
+    th3 = Process(target=test)
+    th3.start()
+    print("테스트 시작")
+
+
+    # th2 = Process(target=sensor.sensor_loop)
+    # th2.start()
+    print("센서 루프 시작")
+
+    # while True:
+    #
+    #     print("Zzz")
+    #     # order2 = transcribe_streaming_mic.order
+    #     print(order)
+    #     if "자동" in order:
+    #         th2 = Process(target=sensor.sensor_loop)
+    #         th2.start()
+    #         print("센서 자동 제어 시작")
+    #     # if "자동 오프" in order2:
+    #     #     th2.terminate()
+    #     #     print("센서 자동 제어 종료")
+    #     time.sleep(1)
+
+
+
+
+
+
+# def work(id, start, end, result):
+#     total = 0
+#     for i in range(start, end):
+#         total += i
+#     result.put(total)
+#     return
+#
+#
+# if __name__ == "__main__":
+#     START, END = 0, 100000000
+#     is_open = 0
+#     th1 = Process(target=work, args=(1, START, END // 2, is))
+#     th2 = Process(target=work, args=(2, END // 2, END, result))
+#
+#     th1.start()
+#     th2.start()
+#     th1.join()
+#     th2.join()
+#
+#     result.put('STOP')
+#     total = 0
+#     while True:
+#         tmp = result.get()
+#         if tmp == 'STOP':
+#             break
+#         else:
+#             total += tmp
+#     print(f"Result: {total}")
