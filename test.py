f4b17b54789d2416322ef25e61ddf2ffc3fa04c2
이효원 <ymj07168@gmail.com>
2022-12-18 04:47:06 +0900

    [Test] 쓰레드 테스트 코드

--- test.py
+++ test.py
@@ -13,6 +13,8 @@ def thread2(threadname):
         while 1:
                 order = "False"
                 time.sleep(1)
+                order = "True"
+                time.sleep(1)
 
 thread1 = Thread(target=thread1, args=("Thread-1", ))
 thread2 = Thread(target=thread2, args=("Thread-2", ))
@@ -20,5 +22,5 @@ thread2 = Thread(target=thread2, args=("Thread-2", ))
 thread1.start()
 thread2.start()
 
-thread1.join()
-thread2.join()
+# thread1.join()
+# thread2.join()
